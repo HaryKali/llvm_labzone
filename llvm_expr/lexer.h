3014e74d40f688045ae544c4a80845fdb1642547
HaryKali <HaryKali@outlook.com>
2026-01-24 21:37:49 +0800

    Add lexer for expr compiler

--- llvm_expr/lexer.h
+++ llvm_expr/lexer.h
@@ -1,41 +1,53 @@
 #pragma once
-#include<llvm/ADT/StringRef.h>
-/// cahr stream -> token stream
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/raw_ostream.h"
 
-enum class TokenType {
-    unknow,
+/// char stream -> Token
+
+enum class TokenType
+{
+    unknown,
     number,
-    plus,
-    minus,
-    slash,
-    star,
-    l_paren,
-    r_paren,
-    semi,
-    eof, // end of file
+    minus,    // -
+    plus,     // +
+    star,     // *
+    slash,    // '/'
+    l_parent, // {
+    r_parent, // }
+    semi,     // ';'
+    eof       // end
 };
 
+class Token
+{
+public:
+    int row, col;
+    TokenType tokenType;
+    int value;
+    llvm::StringRef content;
+    Token()
+    {
+        row = col = -1;
+        tokenType = TokenType::unknown;
+        value = -1;
+    }
 
-class Token {
-    public:
-        int row, col;
-        TokenType tokenType;
-        int value; // only for number tokens
+    void Dump()
+    {
+        llvm::outs() << "{ " << content << ", row = " << row << ", col = " << col << "}\n";
+    }
+};
 
-        Token(){
-            row=col=-1;
-            tokenType=TokenType::unknow;
-            value=0;
+class Lexer
+{
+public:
+    Lexer(llvm::StringRef sourceCode);
 
-        }
-};  
+    void NextToken(Token &tok);
 
-class Lexer {
-    public:
-        Lexer(llvm::StringRef sourceCode);
-        void NextToken(Token &tok);
-    private:
-        const char* BufPtr;
-        const char* LineHeadPtr;
-        const char* BufEnd;
-};      
+private:
+    const char *BufPtr;
+    const char *LineHeadPtr;
+    const char *BufEnd;
+    int row;
+};
--- llvm_expr/main.cc
+++ llvm_expr/main.cc
@@ -1,9 +1,32 @@
 #include <iostream>
-#include <llvm/IR/Module.h>
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/LLVMContext.h>
+// #include <llvm/IR/Module.h>
+// #include <llvm/IR/IRBuilder.h>
+// #include <llvm/IR/LLVMContext.h>
+#include "lexer.h"
+#include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/ErrorOr.h"
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+        printf("Please provide input file.\n");
+        return 0;
+    }
 
-int main() {
-    std::cout << "Hello, LLVM!" << std::endl;
-    return 0;
+    const char *FileName = argv[1];
+
+    static llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> file = llvm::MemoryBuffer::getFile(FileName);
+    if (!file) {
+        llvm::errs() << "Error reading file: " << FileName << "\n";
+        return 0;
+    }
+    std::unique_ptr<llvm::MemoryBuffer> fileBuffer = std::move(*file);
+    Lexer lex(fileBuffer->getBuffer());
+
+    Token tok;
+    while (tok.tokenType != TokenType::eof) {
+        lex.NextToken(tok);
+        tok.Dump();
+    
+    }
+    return 1;
 }

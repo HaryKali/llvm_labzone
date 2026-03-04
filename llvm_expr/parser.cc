ed261d6cb229b22c905a5b157c0f878a599607e9
HaryKali <HaryKali@outlook.com>
2026-03-04 22:05:41 +0800

    Lazy Update

--- llvm_expr/parser.cc
+++ llvm_expr/parser.cc
@@ -1,1 +1,25 @@
 #include "parser.h"
+
+std::shared_ptr<Program> Parser::ParseProgram(){
+    //while Ture .....EXIT
+    //token->eof
+};
+std::shared_ptr<Expr> Parser::ParseTerm(){
+
+};
+std::shared_ptr<Expr> Parser::ParseFactor(){
+
+};
+
+bool Parser::Expect(TokenType tokenType){
+
+
+};
+
+bool Parser::Consume(TokenType tokenType){
+
+};
+
+void Parser::Advance(){
+    // lexer.NextToken(tok);
+};
--- llvm_expr/parser.h
+++ llvm_expr/parser.h
@@ -1,8 +1,24 @@
 #pragma once
 #include "lexer.h"
+#include "ast.h"
 class Parser {
     private:
         Lexer lexer;
+        std::shared_ptr<Expr> ParseTerm();
+        std::shared_ptr<Expr> ParseFactor();
     public:
         Parser(Lexer &lexer) : lexer(lexer) {};
+        std::shared_ptr<Program> ParseProgram();
+
+
+    //Check if the next token is of the expected type.
+    bool Expect(TokenType tokenType);
+    
+    // Check and consume the next token if it matches the expected type. If it does not match, it should report an error.
+    bool Consume(TokenType tokenType);
+    //Advnac
+    void Advance();
+
+    Token tok;
+
 };
